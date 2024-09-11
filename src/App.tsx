6d5e98a3fc43a16338b1b162b0e0c7eeaf6a723a
bvenkataswamy <venkataswamyb07@gmail.com>
2024-09-11 17:40:50 -0500

    Pagination changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import { Heros } from './layouts/HomePage/Components/Heros';
 import { LibraryServices } from './layouts/HomePage/Components/LibraryServices';
 import { Footer } from './layouts/HomePage/Components/Footer';
 import { HomePage } from './layouts/HomePage/Homepage';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
 export const  App = ()  => {
   return (
     <div>
-    <HomePage/>
+   {/* <HomePage/> */}
+    <SearchBooksPage/>
     </div>
   );
 }
---
+++ src/layouts/SearchBooksPage/components/SearchBook.tsx
@@ -0,0 +1,57 @@
+import BookModel from "../../../model/BookModel";
+
+export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
+    return (<div className="card mt-3 shadow p-3 bg-body rounded">
+        <div className="row g-0">
+            <div className="col-md-2">
+                <div className="d-none d-lg-block">
+                    {props.book.img ?
+                        <img src={props.book.img}
+                            width='123'
+                            height='196'
+                            alt="Book" />
+                        :
+                        <img src={require('../../../Images/BooksImages/book-luv2code-1000.png')}
+                            width='123'
+                            height='196'
+                            alt="Book" />
+                    }
+                </div>
+
+                <div className="d-lg-none d-flex justify-content-center align-items-center">
+                    {props.book.img ?
+                        <img src={props.book.img}
+                            width='123'
+                            height='196'
+                            alt="Book" />
+                        :
+                        <img src={require('../../../Images/BooksImages/book-luv2code-1000.png')}
+                            width='123'
+                            height='196'
+                            alt="Book" />
+                    }
+                </div>
+            </div>
+            <div className='col-md-6'>
+                <div className='card-body'>
+                    <h5 className="card-title">
+                        {props.book.author}
+                    </h5>
+                    <h4>
+                        {props.book.title}
+                    </h4>
+                    <p className="card-text">
+                        {props.book.description}
+                    </p>
+                </div>
+                <div className="col-md-5 d-flex justify-content-center align-item-center">
+                    <a className="btn btn-md main-color text-white" href="#">
+                        View Details
+                    </a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
